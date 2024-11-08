f2e12bfad43555f3fd3deb1dfd137720c10cf101
HanzhenX <xhzyunkong@gmail.com>
2024-11-08 11:29:25 -0500

    CKPT: Last working version.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::cell::RefCell;
 
 // Define an enum for different event types
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
-enum EventType {
+enum Event {
     MarketData,
     OrderPlace,
     OrderComplete
@@ -15,15 +15,10 @@ trait EventHandler {
     fn handle_event(&self, event: &Event);
 }
 
-// Event structure containing the event type and any additional data
-struct Event {
-    event_type: EventType,
-    // You can add more fields as needed, e.g., market data, signal info, etc.
-}
 
 // Define an EventManager struct that manages events and handlers
 struct EventManager {
-    subscriber_book: HashMap<EventType, Vec<Rc<RefCell<dyn EventHandler>>>>,
+    subscriber_book: HashMap<Event, Vec<Rc<RefCell<dyn EventHandler>>>>,
     event_queue: VecDeque<Event>,
 }
 
@@ -36,7 +31,7 @@ impl EventManager {
     }
 
     // Subscribe a handler for a specific event type
-    fn subscribe(&mut self, event_type: EventType, handler: Rc<RefCell<dyn EventHandler>>) {
+    fn subscribe(&mut self, event_type: Event, handler: Rc<RefCell<dyn EventHandler>>) {
         self.subscriber_book.entry(event_type).or_insert(Vec::new()).push(handler);
     }
 
@@ -48,7 +43,7 @@ impl EventManager {
     // Process events by dispatching them to the registered handlers
     fn process_events(&mut self) {
         while let Some(event) = self.event_queue.pop_front() {
-            if let Some(handlers) = self.subscriber_book.get(&event.event_type) {
+            if let Some(handlers) = self.subscriber_book.get(&event) {
                 for handler in handlers {
                     handler.borrow().handle_event(&event);
                 }
@@ -59,23 +54,67 @@ impl EventManager {
 
 // Implementing example modules that implement EventHandler trait
 
-struct MarketDataFeeder;
+struct MarketDataFeeder {
+    event_manager: Rc<RefCell<EventManager>>,
+}
 
-struct Strategy;
+impl MarketDataFeeder {
+    fn new(event_manager: Rc<RefCell<EventManager>>) -> Self {
+        MarketDataFeeder { event_manager }
+    }
 
-impl EventHandler for Strategy {
-    fn handle_event(&self, event: &Event) {
-        println!("Strategy handling event: {:?}", event.event_type);
-        // Implement specific logic for handling strategy events
+    // Method to push a MarketData event
+    fn push_event(&self) {
+        let event = Event::MarketData ;
+        self.event_manager.borrow_mut().push_event(event);
     }
 }
 
-struct MockExchange;
+struct Strategy{
+    event_manager: Rc<RefCell<EventManager>>,
+}
+
+impl Strategy {
+    fn new(event_manager: Rc<RefCell<EventManager>>) -> Self {
+        Strategy { event_manager }
+    }
+
+    // Method to push a OrderPlace event
+    fn push_event(&self) {
+        let event = Event::OrderPlace ;
+        self.event_manager.borrow_mut().push_event(event);
+    }
+}
+
+impl EventHandler for Strategy {
+    fn handle_event(&self, event: &Event) {
+        println!("Strategy handling event: {:?}, publish a new OrderPlace", event);
+        // Implement specific logic for handling strategy events
+        // self.push_event();
+    }
+}
+
+struct MockExchange {
+    event_manager: Rc<RefCell<EventManager>>,
+}
+
+impl MockExchange {
+    fn new(event_manager: Rc<RefCell<EventManager>>) -> Self {
+        MockExchange { event_manager }
+    }
+
+    // Push an OrderComplete event
+    fn push_event(&self) {
+        let event = Event::OrderComplete;
+        self.event_manager.borrow_mut().push_event(event);
+    }
+}
 
 impl EventHandler for MockExchange {
     fn handle_event(&self, event: &Event) {
-        println!("OrderHandler handling event: {:?}", event.event_type);
+        println!("OrderHandler handling event: {:?}, publishing OrderComplete", event);
         // Implement specific logic for handling order events
+        // self.push_event();
     }
 }
 
@@ -83,29 +122,32 @@ struct ProtfolioManager;
 
 impl EventHandler for ProtfolioManager {
     fn handle_event(&self, event: &Event) {
-        println!("PortfolioHandler handling event: {:?}", event.event_type);
+        println!("PortfolioHandler handling event: {:?}", event);
         // Implement specific logic for handling portfolio events
     }
 }
 
 fn main() {
-    let mut event_manager = EventManager::new();
+    let event_manager = Rc::new(RefCell::new(EventManager::new()));
+    let market_data_feeder = MarketDataFeeder::new(Rc::clone(&event_manager));
+    let mock_exchange = MockExchange::new(Rc::clone(&event_manager));
+    let my_strategy = Strategy::new(Rc::clone(&event_manager));
 
     // Register different handlers for different event types
-    event_manager.subscribe(EventType::MarketData, Rc::new(RefCell::new(Strategy)));
-    event_manager.subscribe(EventType::MarketData, Rc::new(RefCell::new(MockExchange)));
-    event_manager.subscribe(EventType::OrderPlace, Rc::new(RefCell::new(ProtfolioManager)));
-    event_manager.subscribe(EventType::OrderPlace, Rc::new(RefCell::new(MockExchange)));
-    event_manager.subscribe(EventType::OrderComplete, Rc::new(RefCell::new(MockExchange)));
+    event_manager.borrow_mut().subscribe(Event::MarketData, Rc::new(RefCell::new(Strategy::new(Rc::clone(&event_manager)))));
+    event_manager.borrow_mut().subscribe(Event::MarketData, Rc::new(RefCell::new(MockExchange::new(Rc::clone(&event_manager)))));
+    event_manager.borrow_mut().subscribe(Event::OrderPlace, Rc::new(RefCell::new(ProtfolioManager)));
+    event_manager.borrow_mut().subscribe(Event::OrderPlace, Rc::new(RefCell::new(MockExchange::new(Rc::clone(&event_manager)))));
+    event_manager.borrow_mut().subscribe(Event::OrderComplete, Rc::new(RefCell::new(MockExchange::new(Rc::clone(&event_manager)))));
 
 
     // Push events to the event manager
-    event_manager.push_event(Event { event_type: EventType::MarketData });
-    event_manager.push_event(Event { event_type: EventType::MarketData });
-    event_manager.push_event(Event { event_type: EventType::MarketData });
-    event_manager.push_event(Event { event_type: EventType::OrderPlace });
-    event_manager.push_event(Event { event_type: EventType::OrderComplete });
+    market_data_feeder.push_event();
+    // market_data_feeder.push_event();
+    // market_data_feeder.push_event();
+    my_strategy.push_event();
+    mock_exchange.push_event();
 
     // Process events
-    event_manager.process_events();
+    event_manager.borrow_mut().process_events();
 }
