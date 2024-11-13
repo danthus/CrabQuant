c3134508985be04a4e2720917d06ae7f72083431
Hanzhen Xu <xhzyunkong@gmail.com>
2024-11-13 14:06:03 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-mod events;
 mod event_manager;
+mod events;
 mod market_data_feeder;
-mod strategy;
 mod mock_exchange;
 mod portfolio_manager;
+mod strategy;
 
-use crate::event_manager::{EventManager, EventHandler};
+use crate::event_manager::{EventHandler, EventManager};
 use crate::events::EventType;
 
-use std::sync::{Arc, Mutex};
-use std::thread;
 use market_data_feeder::MarketDataFeeder;
-use strategy::Strategy;
 use mock_exchange::MockExchange;
 use portfolio_manager::{PortfolioManager, PortfolioManagerWrapper};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use strategy::Strategy;
 
 fn main() {
     let event_manager = Arc::new(Mutex::new(EventManager::new()));
     let initial_cash = 1000000.;
     let portfolio_manager = Arc::new(Mutex::new(PortfolioManager::new(initial_cash)));
-    let portfolio_wrapper:Arc<dyn EventHandler> = Arc::new(PortfolioManagerWrapper::new(Arc::clone(&portfolio_manager)));
-    let my_strategy :Arc<dyn EventHandler> =Arc::new(Strategy::new());
-    let mock_exchange :Arc<dyn EventHandler>= Arc::new(MockExchange::new());
+    let portfolio_wrapper: Arc<dyn EventHandler> =
+        Arc::new(PortfolioManagerWrapper::new(Arc::clone(&portfolio_manager)));
+    let my_strategy: Arc<dyn EventHandler> = Arc::new(Strategy::new());
+    let mock_exchange: Arc<dyn EventHandler> = Arc::new(MockExchange::new());
     {
         let mut em = event_manager.lock().unwrap();
         em.subscribe(EventType::MarketData, Arc::clone(&my_strategy));
@@ -31,7 +32,7 @@ fn main() {
     }
 
     let event_sender = event_manager.lock().unwrap().get_event_sender();
-    
+
     let event_manager_clone = Arc::clone(&event_manager);
     thread::spawn(move || {
         EventManager::process_events(event_manager_clone);
