afd2a4758595f91f0f2f7c3bc48859b720ca93e3
Hanzhen Xu <xhzyunkong@gmail.com>
2024-12-13 16:38:55 -0500

    Performant update on DA

--- src/data_analyzer.rs
+++ src/data_analyzer.rs
@@ -4,24 +4,16 @@ use crossbeam::channel::{bounded, unbounded, Receiver, Sender};
 use num_traits::cast::ToPrimitive;
 use plotters::prelude::*;
 use std::error::Error;
-#[cfg(feature= "order_test")]
-use crate::util::Counter;
-use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::thread;
-use rand::Rng;
 use std::time::Duration;
 
-
 pub struct DataAnalyzer {
-    // subscribe_sender is for event_manager to use only.
-    // as DA is not supposed to block any other events,
-    // it will use a unbounded channel.
     subscribe_sender: Sender<Event>,
     subscribe_receiver: Receiver<Event>,
-    market_data: Vec<(String, f64)>,
-    // local_portfolio: use this to parse PortfolioInfoEvents
+    market_data_history: Arc<Mutex<Vec<(String, f64)>>>,
+    asset_history: Arc<Mutex<Vec<(String, f64)>>>,
     local_portfolio: Portfolio,
-    asset_history: Vec<(String, f64)>,
 }
 
 impl ModuleReceive for DataAnalyzer {
@@ -33,44 +25,59 @@ impl ModuleReceive for DataAnalyzer {
 impl DataAnalyzer {
     pub fn new() -> Self {
         let (subscribe_sender, subscribe_receiver) = unbounded();
-        let market_data = Vec::new();
+        let market_data_history = Arc::new(Mutex::new(Vec::new()));
+        let asset_history = Arc::new(Mutex::new(Vec::new()));
         let local_portfolio = Portfolio::new(0.0);
-        let asset_history = Vec::new();
+
+        // Spawn a new thread for plotting
+        let market_data_history_clone = Arc::clone(&market_data_history);
+        let asset_history_clone = Arc::clone(&asset_history);
+
+        thread::spawn(move || {
+            let mut last_lengths = (0, 0); // Track lengths of histories
+            loop {
+                // Take a snapshot of data
+                let market_data_snapshot = {
+                    let data = market_data_history_clone.lock().unwrap();
+                    data.clone()
+                };
+                let asset_history_snapshot = {
+                    let data = asset_history_clone.lock().unwrap();
+                    data.clone()
+                };
+
+                // Plot and save the data if there are updates
+                if let Err(err) = DataAnalyzer::plot(
+                    &market_data_snapshot,
+                    &asset_history_snapshot,
+                    &mut last_lengths,
+                    "performance.png",
+                ) {
+                    eprintln!("Error during plotting: {}", err);
+                }
+
+                thread::sleep(Duration::from_secs(1)); // Reduce resource usage
+            }
+        });
+
         DataAnalyzer {
             subscribe_sender,
             subscribe_receiver,
-            market_data,
-            local_portfolio,
+            market_data_history,
             asset_history,
+            local_portfolio,
         }
     }
 
-    pub fn run(&mut self) -> () {
-        #[cfg(feature= "order_test")]
-        let mut counter_a = Counter::new();
-        #[cfg(feature= "order_test")]
-        let mut counter_b = Counter::new();
-        #[cfg(feature= "order_test")]
-        let mut counter_c = Counter::new();
-        #[cfg(feature= "random_sleep_test")]
-        let mut rng = rand::thread_rng();
-
-        // Control Loop
+    pub fn run(&mut self) {
         loop {
             let event = self.subscribe_receiver.recv().unwrap();
-
             match event {
                 Event::MarketData(market_data_event) => {
-                    // self.process_marketevent(market_data_event);
-                    self.process_marketevent(market_data_event).unwrap_or_else(|err| {
-                        eprintln!("Error updating plot: {}", err);
-                    });
+                    self.process_marketevent(market_data_event);
                 }
                 Event::PortfolioInfo(portfolio_info_event) => {
-                    // self.process_portfolioinfo(portfolio_info_event);
-                    self.process_portfolioinfo(portfolio_info_event).unwrap_or_else(|err| {
-                        eprintln!("Error updating plot: {}", err);
-                    });
+                    self.process_portfolioinfo(portfolio_info_event);
                 }
                 _ => {
                     println!("DataAnalyzer: Unsupported event: {:?}", event);
@@ -79,50 +86,64 @@ impl DataAnalyzer {
         }
     }
 
-    fn process_marketevent(&mut self, market_data_event: MarketDataEvent) -> Result<(), Box<dyn Error>> {
-        // TODO: use marketdata event to plot baseline
-        // Or maintain a vec to update asset (currently to be handled by mockexchange)
-        println!("DA: Updating event: {:?}", market_data_event);
-        self.market_data.push((market_data_event.timestamp.clone(), market_data_event.close));
-        // if self.market_data.len()>10 {println!("market_data: {:?}", &self.market_data);}
-        // self.plot(&self.market_data, "baseline.png")?;
-        Ok(())
+    fn process_marketevent(&mut self, market_data_event: MarketDataEvent) {
+        let mut market_data_history = self.market_data_history.lock().unwrap();
+        market_data_history.push((market_data_event.timestamp.clone(), market_data_event.close));
+        println!("DA: Updated market data history: {:?}", market_data_event);
     }
 
-    fn plot(&self, market_data: &[(String, f64)], asset_history: &[(String, f64)], output_path: &str) -> Result<(), Box<dyn Error>> {
+    fn process_portfolioinfo(&mut self, portfolio_info_event: PortfolioInfoEvent) {
+        self.local_portfolio = portfolio_info_event.portfolio.clone();
+        let mut asset_history = self.asset_history.lock().unwrap();
+
+        if let Some((latest_timestamp, _)) = self.market_data_history.lock().unwrap().last() {
+            asset_history.push((latest_timestamp.clone(), self.local_portfolio.asset));
+        }
+        println!("DA: Updated asset history: {:?}", self.local_portfolio);
+    }
+
+    fn plot(
+        market_data: &[(String, f64)],
+        asset_history: &[(String, f64)],
+        last_lengths: &mut (usize, usize), // Track the lengths of the histories
+        output_path: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        // Check if there are updates
+        if market_data.len() == last_lengths.0 && asset_history.len() == last_lengths.1 {
+            // No updates, skip plotting
+            println!("No updates in data, skipping plot.");
+            return Ok(());
+        }
+
+        // Update last_lengths to the current lengths
+        last_lengths.0 = market_data.len();
+        last_lengths.1 = asset_history.len();
+
         if market_data.is_empty() && asset_history.is_empty() {
             eprintln!("No data points available for plotting.");
             return Ok(());
         }
-        // Get the first market data value to standardize
-        let first_market_value = market_data[0].1;
-        let first_asset_value = asset_history[0].1;
-        
-        // Standardize market data
+
+        let first_market_value = market_data.get(0).map_or(1.0, |(_, value)| *value);
+        let first_asset_value = asset_history.get(0).map_or(1.0, |(_, value)| *value);
+
         let standardized_market_data: Vec<(String, f64)> = market_data
             .iter()
             .map(|(timestamp, value)| (timestamp.clone(), value / first_market_value))
             .collect();
 
-        // Standardize asset history
         let standardized_asset_history: Vec<(String, f64)> = asset_history
             .iter()
             .map(|(timestamp, value)| (timestamp.clone(), value / first_asset_value))
             .collect();
 
-        // Calculate the y-axis range for standardized data
         let all_y_values = standardized_market_data
             .iter()
             .map(|&(_, value)| value)
             .chain(standardized_asset_history.iter().map(|&(_, value)| value));
         let y_min = all_y_values.clone().fold(f64::INFINITY, f64::min);
         let y_max = all_y_values.fold(f64::NEG_INFINITY, f64::max);
-        // if data.len()>10 {
-        //     println!("ymin: {}", y_min);
-        //     println!("ymax: {}", y_max);
-        // }
 
-        // Set up the drawing area
         let root_area = BitMapBackend::new(output_path, (1280, 720)).into_drawing_area();
         root_area.fill(&WHITE)?;
 
@@ -135,19 +156,12 @@ impl DataAnalyzer {
         // Configure the mesh
         chart
             .configure_mesh()
-            // .x_labels(11)
-            // .y_labels(11)
             .x_desc("Date")
             .y_desc("Value")
             .axis_desc_style(("sans-serif", 18))
             .x_label_formatter(&|x| {
                 if let Some(index) = x.to_usize() {
-                    // println!("index: {}", index);
-                    // println!("datalen: {}", data.len());
                     if index < standardized_market_data.len() {
-                        // Extract the date portion from the timestamp
-                        // println!("index: {}", index);
-                        // println!("x: {}", data[index].0.to_string());
                         return standardized_market_data[index].0.clone();
                     }
                 }
@@ -158,7 +172,10 @@ impl DataAnalyzer {
         // Plot the market data (close prices) in blue
         chart
             .draw_series(LineSeries::new(
-                standardized_market_data.iter().enumerate().map(|(i, &(_, close))| (i, close)),
+                standardized_market_data
+                    .iter()
+                    .enumerate()
+                    .map(|(i, &(_, close))| (i, close)),
                 &BLUE,
             ))?
             .label("Market Data")
@@ -168,13 +185,13 @@ impl DataAnalyzer {
         chart
             .draw_series(LineSeries::new(
                 standardized_asset_history
-            .iter()
-            .enumerate()
-            .map(|(i, &(_, asset))| (i, asset)),
-        &RED,
+                    .iter()
+                    .enumerate()
+                    .map(|(i, &(_, asset))| (i, asset)),
+                &RED,
             ))?
-        .label("Asset Value")
-        .legend(|(x, y)| PathElement::new([(x, y), (x + 20, y)], &RED));
+            .label("Asset Value")
+            .legend(|(x, y)| PathElement::new([(x, y), (x + 20, y)], &RED));
 
         // Draw the legend
         chart
@@ -183,28 +200,8 @@ impl DataAnalyzer {
             .border_style(&BLACK)
             .draw()?;
 
-        // println!("Real-time plot updated: {}", output_path);
-        // println!("Plot updated with {} data points.", data.len());
-        // drop(root_area);
-        Ok(())
-    }
+        println!("Plot updated: {}", output_path);
 
-    fn process_portfolioinfo(&mut self, portfolio_info_event: PortfolioInfoEvent) -> Result<(), Box<dyn Error>> {
-        println!("DA: Updating event: {:?}", portfolio_info_event);
-        self.local_portfolio = portfolio_info_event.portfolio.clone();            
-        
-        // TODO: update assets vector
-        // Use the timestamp of the last market_data
-        if let Some((latest_timestamp, _)) = self.market_data.last() {
-        // Add to asset_history with the latest market_data timestamp
-            self.asset_history.push((latest_timestamp.clone(), self.local_portfolio.asset));
-
-        // Plot the asset_history
-            if portfolio_info_event.id==2516 {self.plot(&self.market_data, &self.asset_history, "performance.png")?};
-        } else {
-            eprintln!("No market data available to synchronize with portfolio info!");
-            return Ok(());
-        }
         Ok(())
     }
 }
